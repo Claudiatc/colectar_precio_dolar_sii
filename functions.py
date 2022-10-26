655ac07402c9cbfd6a917577688f458dfb6791b9
clautc <claudia.tejos@gmail.com>
2022-10-26 10:37:53 -0300

    cambio nombre script clase

--- functions.py
+++ functions.py
@@ -1,34 +1,45 @@
 import os
 import errno
 
-def crear_dir_data():
-    try:
-        os.mkdir('data')
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
+class ProcesarData:
+
+    def __int__(self):
+        try:
+            os.mkdir('data')
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+        try:
+            os.mkdir('output/images')
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
 
 
-def mes_to_month(df, var):
-    mes_month = {'mes': ['Ene', 'Feb',
-                 'Mar',  'Abr',
-                 'May',  'Jun',
-                 'Jul',  'Ago',
-                 'Sep',  'Oct',
-                 'Nov',  'Dic'],
-                 'month': ['January','February',
-                  'March', 'April',
-                  'May',   'June',
-                  'July',  'August',
-                  'September', 'October',
-                  'November',  'December']}
-    df[var] = df[var].replace(mes_month['mes'], mes_month['month'])
-    return df
+    def mes_to_month(self, df, var):
+        mes_month = {'mes': ['Ene', 'Feb',
+                     'Mar',  'Abr',
+                     'May',  'Jun',
+                     'Jul',  'Ago',
+                     'Sep',  'Oct',
+                     'Nov',  'Dic'],
+                     'month': ['January','February',
+                      'March', 'April',
+                      'May',   'June',
+                      'July',  'August',
+                      'September', 'October',
+                      'November',  'December']}
+        df[var] = df[var].replace(mes_month['mes'], mes_month['month'])
+        return df
 
 
-def remplazar_coma_punto(df):
-    df['dolar'] = df.dolar.str.replace(',', '.')
-    df['prom'] = df.prom.str.replace(',', '.')
-    return df
+    def remplazar_coma_punto(self, df, vars):
+        if len(vars) > 1:
+            for v in vars:
+                df[v] = df[v].str.replace(',', '\.', regex=True)
+        else:
+            df[vars] = df[vars].str.replace(',', '\.', regex=True)
 
-#%%
+        return df
+
+
