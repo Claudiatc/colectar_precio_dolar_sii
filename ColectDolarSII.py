a941cc064dfe8c24441f4c8c1acdca96c95557da
clautc <claudia.tejos@gmail.com>
2022-10-27 15:56:42 -0300

    corrección de clase dolar para unir todos los años en una tabla

--- ColectDolarSII.py
+++ ColectDolarSII.py
@@ -6,16 +6,37 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from time import sleep
+from collections import OrderedDict
 
 
+class ColectarDolarSII:
 
-class ColectDolarSII:
+    def __init__(self):
 
-    def __init__(self, name, metadata):
+        metadata = {'info':
+                        """PENDIENTE:
+                Desde home de sii, se puede seleccionar dólar y llegar a un menú de filtros para meses y años.
+                Al seleccionar año, se actualiza el drive y quedan obsoletos los elementos.
+                Intenté con iframe, pero no hay iframe a la vista.
+                Intenté con for e ir actualizando los elementos, pero el mensaje de error igual aparece:
+                element is not attached to the page document 
+                SOLUCIÓN TEMPORAL: usar url específicos al valor de dólar por día.
+                """,
+                    'web': 'https://www.sii.cl/valores_y_fechas/dolar/dolar',
+
+                    'web_ext': '.htm',
+                    'path': '/usr/bin/chromedriver_linux/chromedriver',
+                    'id_sel_mes': 'sel_mes',
+                    'valor_mes': 'todos',
+                    'year': '//*[@id="my-wrapper"]/div[2]/div[1]/div/div/div[2]/div[1]/div/div[2]/div/div/button/span[1]',
+                    'encabezados': '//*[@id="table_export"]/thead/tr/th',
+                    'valores': '//*[@id="table_export"]/tbody/tr'
+                    }
+
+        self.anios = ['2021', '2022']
         self.metadata = metadata
-        self.name = name
-        print('Hola {}, te iré contando el proceso'.format(name))
-
+        self.name = 'Clau'
+        print('Hola {}, te iré contando el proceso'.format(self.name))
 
     def crear_sesion(self, seg):
         print('se crea sesión por cada año consultado, porque Selenium trabaja con elementos web actualizados.')
@@ -23,8 +44,12 @@ class ColectDolarSII:
         wait = WebDriverWait(driver, seg)
         return driver, wait
 
-    def construir_pag_web(self, str_year):
-        print('Según el número de objetos ingresados en tu lista de años, se creará una url para consultar información sobre precio del dolar')
+    def construir_pag_web(self, str_year=None):
+
+        if str_year is None:
+            str_year = self.anios
+        print(
+            'Según el número de objetos ingresados en tu lista de años, se creará una url para consultar información sobre precio del dolar')
         """
         :param:
         str_year:list
@@ -34,7 +59,6 @@ class ColectDolarSII:
         website: list
         lista de páginas web construidas según años requeridos
         """
-
         website: List[str] = []
         if len(str_year) <= 1:
             website = [''.join([self.metadata['web'], str_year[0], self.metadata['web_ext']])]
@@ -93,15 +117,16 @@ class ColectDolarSII:
         :param website: lista de sitios webs a scrapear
         :return: tabla
         """
-        enc = []
-        val = []
-        val_filas = []
 
         if len(website) > 1:
 
             dfs = pd.DataFrame()
+            d = pd.DataFrame()
 
             for w in website:
+                enc = []
+                val = []
+                val_filas = []
                 driver, wait = self.crear_sesion(15)
                 driver.get(w)
                 selec_mes = Select(driver.find_element(By.ID, self.metadata['id_sel_mes']))
@@ -111,6 +136,7 @@ class ColectDolarSII:
                 for e in encabezados:
                     enc.append(e.text)
                 print(enc)
+
                 valores = driver.find_elements(By.XPATH, self.metadata['valores'])
 
                 for v in valores:
@@ -119,16 +145,24 @@ class ColectDolarSII:
 
                 for vf in val:
                     val_filas.append(vf.replace('  ', ' ').split(' '))
-                print(val_filas)
+
+                # print(val_filas)
 
                 filas = pd.DataFrame(val_filas)
-                filas.columns = enc
-                df = self.remodelar_tabla(filas, driver)
+                filas.columns = list(OrderedDict.fromkeys(enc))
+
+                d = self.remodelar_tabla(filas, driver)
                 driver.close()
-                dfs = pd.concat([dfs, df], join='outer', ignore_index=True)
-                return dfs
+                dfs = pd.concat([dfs, d], join='outer', ignore_index=True)
+                dfs = dfs[dfs['day'] != 32]
+                dfs = dfs.reset_index(drop=True)
+
+            return dfs
 
         else:
+            enc = []
+            val = []
+            val_filas = []
             driver, wait = self.crear_sesion(15)
             driver.get(website[0])
             selec_mes = Select(driver.find_element(By.ID, self.metadata['id_sel_mes']))
@@ -155,5 +189,3 @@ class ColectDolarSII:
 
             driver.close()
             return df
-
-#%%
